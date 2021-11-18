9e0df45ebc588351fc68196f117d2462b99f091e
a.sigarev <a.sigarev@fingineers.ru>
2021-11-18 16:26:41 +0300

    init

---
+++ app/src/main/java/ru/bsc/contemporaryNotes/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.bsc.contemporaryNotes.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.bsc.contemporaryNotes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.frag_creating_note)
+    }
+}
