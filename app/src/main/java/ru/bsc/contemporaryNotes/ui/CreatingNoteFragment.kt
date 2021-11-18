bb97bc74ded02bcdb8aac05bb9c5381110dd34fb
a.sigarev <a.sigarev@fingineers.ru>
2021-11-18 16:32:48 +0300

    add ui frag

---
+++ app/src/main/java/ru/bsc/contemporaryNotes/ui/CreatingNoteFragment.kt
@@ -0,0 +1,18 @@
+package ru.bsc.contemporaryNotes.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.bsc.contemporaryNotes.R
+
+
+class CreatingNoteFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.frag_creating_note, container, false)
+    }
+}
