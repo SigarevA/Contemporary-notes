3d7d0c27dc8afd793d017d943ea1078df67616a3
Sigarev Aleksei <afakusima@mail.ru>
2021-12-04 22:29:05 +0300

    add block reminder to creating note ui

--- app/src/main/java/ru/bsc/contemporaryNotes/ui/MainActivity.kt
+++ app/src/main/java/ru/bsc/contemporaryNotes/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.bsc.contemporaryNotes.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
